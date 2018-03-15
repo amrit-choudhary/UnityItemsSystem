6a599db2a0a741d736b23d1c3d1f141ae26a2f5e
amrit-choudhary <albela.amrit@gmail.com>
2018-03-16 01:20:13 +0530

    test code for item parameter class

---
+++ Assets/UnityItemsSystem/Scripts/Test/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    TestParam health1 = new TestParam(6, "Health 1");
+    TestParam health2 = new TestParam(3, "Health 2");
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log(health1.ID);
+        Debug.Log(health2.ID);
+
+        if (health1 != health2) {
+            Debug.Log("health not equal");
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/UnityItemsSystem/Scripts/Test/TestParam.cs
@@ -0,0 +1,45 @@
+﻿
+public class TestParam {
+
+    private int _value;
+    private string _id;
+    public string ID {
+        get{
+            return _id;
+        }
+    }
+
+    public TestParam(int value, string id) {
+        _value = value;
+        _id = id;
+    }
+
+    public void TestFn() {
+
+    }
+
+    public static implicit operator int(TestParam testParam) {
+        return testParam._value;
+    }
+
+    public override string ToString() {
+        return _value.ToString();
+    }
+
+    public static bool operator ==(TestParam b1, TestParam b2) {
+        return (b1._value == b2._value);
+    }
+
+    public static bool operator !=(TestParam b1, TestParam b2) {
+        return !(b1 == b2);
+    }
+
+    public override bool Equals(object obj) {
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode() {
+        return base.GetHashCode();
+    }
+
+}
